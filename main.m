55ecd038afe0981ed2b0b0c71eae76e5a4893885
Noah Mulfinger <noah.mulfinger@gmail.com>
2015-12-18 12:09:48 -0800

    provides user with images to colorize, does scribble colorization

--- main.m
+++ main.m
@@ -6,53 +6,59 @@ file_names = dir([input_dir '*.' file_ext]);
 
 
 
-% Get set of images for each scene
+%% Get set of images for each scene (each column is a scene)
 scenes = separate_scenes(input_dir, file_names);
 
-% Get image in center of each scene
-num_scenes = size(scenes, 2);
-images_col = [{''}];
+disp('Number of scenes:')
+num_scenes = size(scenes, 2)
+max_imgs_scene = size(scenes, 1);
+
+
+%% Get image in center of each scene
+images_col = [];
 
 for i = 1:num_scenes
-    scene_set = scenes(:,i)
+    first_image = scenes(1,i);
+    second_image = scenes(1,i);
+    k = strfind(char(second_image), '.png');
+    
+    counter = 1;
+    while ~isempty(second_image{1}) && counter < max_imgs_scene
+        counter = counter + 1;
+        second_image = scenes(counter,i);
+    end
+    second_image = scenes(counter-1,i);
+    
+    first_image = char(first_image);
+    first_val = str2num(first_image(1:end-4));
+    second_image = char(second_image);
+    second_val = str2num(second_image(1:end-4));
+    
+    mid_val = floor(first_val + (second_val - first_val) / 2);
+    img = ['0000' num2str(mid_val) '.png'];
+    images_col = [images_col; img];
+    
     
 end
 
-
-% List these scenes as ones that need to be colorized
-
-disp('Colorize the above images in an image editor, press any key when done')
+%% List these scenes as ones that need to be colorized
+%List of images to colorize
+images_col
+disp('Colorize the above images in an image editor. Label them <file_name>_scr.png and store in root dir')
 pause
 
-% User does scribbling on images
+%% Colorize each image based on user scribbles, then apply to each scene
+for i = 1:size(images_col,1)
+    img = images_col(i,:);
+    img_scr = [img(1:end-4) '_scr.png'];
+    
+    img = imread(img);
+    img_scr = imread(img_col);
+    
+    img_col = colorizeFun(img, img_scr);
+    
+    % Do colorization by example for each scene using img_col as example
+    for j = scenes(:,i)
 
-% Use these images to colorize whole scene
-
-
-
-
-
-
-
-% border_images = zeros(7,2);
-
-
-% num_scenes = size(border_images, 1);
-% scenes = [];
-% 
-% edge = '00001945.png';
-% cur_scene = 1;
-% add_loc_1 = 1;
-% add_loc_2 = 1;
-% for file = file_names'
-%     if str2num(file.name(1:end-4)) < str2num(test(1:end-4))
-%         scenes(add_loc_1,1) = file.name;
-%         add_loc_1 = add_loc_1 + 1;
-%     else
-%         scenes(add_loc_2,2) = file.name;
-%         add_loc_2 = add_loc_2 + 1;
-%     end
-%     
-% end
-% 
-% scenes
+    end
+end
--- test.m
+++ test.m
@@ -1,3 +1,46 @@
-disp('Colorize the above images in an image editor, press any key when done')
-pause
-b = 'hello'
+% x = {'hello.png'};
+% y = '.png';
+% k = strfind(char(x), y);
+% 
+% if ~isempty(k)
+%     disp('hello')
+% end
+% 
+% images_col = '00003435.png';
+% first_val = 2323;
+% second_val = 2324;
+% 
+%  mid_val = floor(first_val + (second_val - first_val) / 2);
+%  img = ['0000' num2str(mid_val) '.png'];
+%  images_col = [images_col; img]
+%  
+%  
+ 
+%  A = [{'111.png'} {'114.png'}; {'112.png'} []; {'113.png'} []; {'114.png'} []; {'115.png'}];
+%  s = size(A, 1)
+%  
+%  first_image = A(1,1);
+%  second_image = A(1,1);
+%  k = strfind(char(second_image), '.png');
+%     
+%  counter = 1;
+%  while ~isempty(k) && counter < s
+%      counter = counter + 1;
+%      second_image = A(counter,1);
+%      k = strfind(char(second_image), '.png');
+%  end
+%  
+%  first_image
+%  second_image
+% 
+
+
+a = [ 1 ; 3 ;4; 5; 6; 8; 7];
+for x = a'
+    x
+end
+
+
+
+
+    
