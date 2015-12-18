14b1c16147819cbaa0901771d7cc97e5721c9e23
Noah Mulfinger <noah.mulfinger@gmail.com>
2015-12-17 23:28:34 -0800

    working on picking images from each scene

--- main.m
+++ main.m
@@ -4,7 +4,32 @@ input_dir = 'medium-sample/';
 file_ext = 'png';
 file_names = dir([input_dir '*.' file_ext]);
 
+
+
+% Get set of images for each scene
 scenes = separate_scenes(input_dir, file_names);
+
+% Get image in center of each scene
+num_scenes = size(scenes, 2);
+images_col = [{''}];
+
+for i = 1:num_scenes
+    scene_set = scenes(:,i)
+    
+end
+
+
+% List these scenes as ones that need to be colorized
+
+disp('Colorize the above images in an image editor, press any key when done')
+pause
+
+% User does scribbling on images
+
+% Use these images to colorize whole scene
+
+
+
 
 
 
@@ -12,7 +37,6 @@ scenes = separate_scenes(input_dir, file_names);
 % border_images = zeros(7,2);
 
 
-% Get set of images for each scene
 % num_scenes = size(border_images, 1);
 % scenes = [];
 % 
@@ -32,11 +56,3 @@ scenes = separate_scenes(input_dir, file_names);
 % end
 % 
 % scenes
-
-% Get image in center of each scene
-
-% List these scenes as ones that need to be colorized
-
-% User does scribbling on images
-
-% Use these images to colorize whole scene
---
+++ test.m
@@ -0,0 +1,3 @@
+disp('Colorize the above images in an image editor, press any key when done')
+pause
+b = 'hello'
